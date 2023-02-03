60f6310ccde891ba96ded3bf846e86a40d51f301
Sodik Owolabi <siresodmond@gmail.com>
2023-02-03 11:55:49 +0100

    Update sign-up.blade.php

--- resources/views/sign-up.blade.php
+++ resources/views/sign-up.blade.php
@@ -185,6 +185,7 @@
                     var email = $('#chk-email').val();
                     $('#chk-email-btn').attr('disabled');
                     $('#email-chckin').css('display', 'block');
+                    alert($('#chk-email-url').val() + '/' + email);
                     $.ajax({
                         type: 'GET',
                         url: 'check_cust/' + email,
